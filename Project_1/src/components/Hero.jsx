0370d54b3af8e6c241cec0d260fae88008b4eb7c
Tanusha6006 <tanushajain74@gmail.com>
2025-04-20 16:12:53 -1200

    Project_1

---
+++ Project_1/src/components/Hero.jsx
@@ -0,0 +1,29 @@
+const Hero = () =>{
+    return(
+        <main className="hero container">
+            <div className = "hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+
+                <div className="hero-btn">
+                    <button> Shop Now </button>
+                    <button className="secondary-btn"> Category </button>
+                </div>
+
+                <div className="shopping">
+                    <p>Also Available On</p>
+
+                    <div className="brand-icon">
+                        <img src="./public/Images/flipkart.png" alt="flipkart logo" />
+                        <img src="./public/Images/amazon.png" alt="Amazon logo" />
+                    </div>
+                </div>
+            </div>
+            <div className = "hero-image">
+                <img src="./public/Images/shoe_image.png" alt="Shoe image" />
+            </div>
+        </main>
+    );
+};
+
+export default Hero;
