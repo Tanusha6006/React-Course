5c5e2b86c84b3f75719413d0a0ed23d1a3d5a6ab
Tanusha6006 <tanushajain74@gmail.com>
2025-05-18 19:43:34 +0530

    Project_3

---
+++ Project_3/src/Components/GamePlay.jsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import { useState } from "react";
+import {Button, OutlineButton} from "../styled/Button"
+import Rules from "./Rules"
+
+import TotalScore from "./TotalScore";
+import NumberSelector from "./NumberSelector";
+import RollDice from "./RollDice";
+const GamePlay = () => {
+  const [score,setScore] = useState(0);
+  const [selectedNumber, setSelectedNumber] = useState();
+  const [currentDice, setCurrentDice] = useState(1);
+  const [error,setError] = useState("");
+  const [showRules, setShowRules] = useState(false)
+
+
+  const generaterandomNumber = (min,max) => {
+    return Math.floor(Math.random() * (max-min) + min);
+  };
+
+  const roleDice = () => {
+    if(!selectedNumber) {
+      setError("Please select a Number from here");
+      return;
+    }
+    
+    const randomNumber = generaterandomNumber(1,7);
+    setCurrentDice(() => randomNumber);
+
+    
+
+    if(selectedNumber === randomNumber){
+      setScore(prev => prev+randomNumber)
+    }
+    else{
+      setScore(prev => prev-2);
+    }
+
+    setSelectedNumber(undefined);
+  }
+
+  const resetScore = () => {
+    setScore(0);
+  }
+
+  return <MainContainer>
+    <div className="top_section">
+      <TotalScore score={score}/>
+      <NumberSelector error={error} setError={setError} selectedNumber={selectedNumber} setSelectedNumber={setSelectedNumber}/>
+    </div>
+    <RollDice currentDice = {currentDice} roleDice = {roleDice}/>
+    <div className="btns">
+      <OutlineButton onClick={resetScore}>Reset Score</OutlineButton>
+      <Button onClick={() => setShowRules((prev) => !prev)}>{showRules?"Hide" : "Show"} Rules</Button>
+    </div>
+
+    {showRules && <Rules/>}
+    
+  </MainContainer>
+}
+
+export default GamePlay;
+
+const MainContainer = styled.div`
+padding-top: 70px;
+  .top_section{
+    display:flex;
+    justify-content: space-around;
+    align-items: end;
+  }
+
+  .btns{
+    margin-top: 40px;
+    gap: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding-right:280px;
+  }
+`;
